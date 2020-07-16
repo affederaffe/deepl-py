f71947f00188de13a98fbeb71234df5069e6ff1f
affederaffe <68356204+affederaffe@users.noreply.github.com>
2020-07-16 20:33:57 +0200

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,37 +1,48 @@
-import setuptools
+from setuptools import setup, find_packages
+import pathlib
 
-with open("README.md", "r") as fh:
+here = pathlib.Path(__file__).parent.resolve()
 
-    long_description = fh.read()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-setuptools.setup(
+setup(
 
-    name="<deepl-py>",
+    name='deepl-py',
 
-    version="0.0.1",
+    version='0.0.1',
 
-    author="<affederaffe>",
-
-    description="<A deepl translator integration made with selenium>",
+    description='A deepl translator integration made with selenium',
 
     long_description=long_description,
 
-    long_description_content_type="text/markdown",
+    url='https://github.com/affederaffe/deepl-py',
 
-    url="<https://github.com/affederaffe/deepl-py>",
-
-    packages=setuptools.find_packages(),
+    author='affederaffe',
 
     classifiers=[
+        'Development Status :: 3 - Alpha',
 
-        "Programming Language :: Python :: 3",
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
 
-        "License :: OSI Approved :: MIT License",
+        'License :: OSI Approved :: MIT License',
 
-        "Operating System :: OS Independent",
-
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
     ],
 
-    python_requires='>=3.6',
+    keywords='deepl, translator, development',
 
+    py_modules=["deepl.py"],
+
+    python_requires='>=3.5, <4',
+
+    project_urls={
+        'Bug Reports': 'https://github.com/affederaffe/deepl-py/issues',
+        'Source': 'https://github.com/affederaffe/deepl-py',
+    },
 )
