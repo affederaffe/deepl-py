47af3b436b8694e5921f1bf4ccaf66b4fb26bd8d
affederaffe <68356204+affederaffe@users.noreply.github.com>
2020-07-16 20:16:58 +0200

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name="<deepl-py>",
+
+    version="1.0.0",
+
+    author="<affederaffe>",
+
+    description="<A deepl translator integration made with selenium>",
+
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
+
+    url="<https://github.com/affederaffe/deepl-py>",
+
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: OS Independent",
+
+    ],
+
+    python_requires='>=3.6',
+
+)
